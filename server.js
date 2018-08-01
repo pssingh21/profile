34af0fbe5a42d4b6554b304cd6021c7ac1258cf9
Peeyush Singh <peeyushmansingh@gmail.com>
2018-08-01 10:59:17 +0545

    changes to .gitignore and server.js

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/profile'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'../dist/profile/index.html'));
+app.get('/*', function(req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+    // res.sendFile(path.join(__dirname+'/dist/profile/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
